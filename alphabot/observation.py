ebee2ce1a89fcfb955abd15cbc9ddc14ede85e50
PatrykNeubauer <patryk.neubauer@gmail.com>
2021-05-28 00:05:34 +0200

    Add Observation class

--- alphabot/observation.py
+++ alphabot/observation.py
@@ -1,5 +1,29 @@
 """Module for Observation class."""
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from numpy import ndarray
+    import preprocessor
 
 
 class Observation:
-    pass
+    """Class for environment's returns."""
+
+    state: "ndarray"
+    reward: float
+    is_finished: bool
+    info: dict
+
+    def __init__(self, state, reward, is_finished, info=None):
+        self.state = state
+        self.reward = reward
+        self.is_finished = is_finished
+        self.info = info
+
+    def preprocess(self):
+        """Preprocesses the information obtained from the environment."""
+        preprocessed_observation = preprocessor.preprocess(self)
+        self.state = preprocessed_observation.state
+        self.reward = preprocessed_observation.reward
+        self.is_finished = preprocessed_observation.is_finished
+        self.info = preprocessed_observation.info
